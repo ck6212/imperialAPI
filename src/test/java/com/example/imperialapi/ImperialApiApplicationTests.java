b66678fe134d024393d192e1182a33cf3a3ae115
Can Karapinar <Karapinar.can@hotmail.com>
2022-06-22 14:39:44 +0200

    added mock tests

--- src/test/java/com/example/imperialapi/ImperialApiApplicationTests.java
+++ src/test/java/com/example/imperialapi/ImperialApiApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.imperialapi;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ImperialApiApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void inchToCentimeter() throws Exception{
+        this.mockMvc.perform(get("/api/centimeter?inch=1")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("2.54")));
+    }
+
+    @Test
+    void yardToMeter() throws Exception{
+        this.mockMvc.perform(get("/api/meter?yard=1")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("0.91")));
     }
 
 }
